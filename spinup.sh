0121dd834c7a87f5c12b1ff6192830028af2ea8f
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-03-18 18:50:05 -0700

    Update spinup.sh

--- spinup.sh
+++ spinup.sh
@@ -1,9 +1,9 @@
-gcloud compute instances create rsyslog1 \
+gcloud compute instances create nfs-server \
         --image-family centos-7 \
         --image-project centos-cloud \
         --zone us-central1-a \
         --tags "http-server","https-server" \
         --machine-type f1-micro \
         --scopes cloud-platform \
-        --metadata-from-file startup-script=/path/to/script.sh=/home/bloshakoul/NTI-310/logserver/logsrv.sh \
-        --private-network-ip=10.138.0.4
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/nfs-server \
+        --private-network-ip=10.168.0.4
