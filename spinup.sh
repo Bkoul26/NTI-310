7ae994d2e8742081c2c15176956f2539687b7656
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-03-18 18:10:04 -0700

    Update spinup.sh

--- spinup.sh
+++ spinup.sh
@@ -6,4 +6,4 @@ gcloud compute instances create rsyslog1 \
         --machine-type f1-micro \
         --scopes cloud-platform \
         --metadata-from-file startup-script=/path/to/script.sh=/home/bloshakoul/NTI-310/logserver/logsrv.sh \
-        --private-network-ip=10.138.0.4
+        --private-network-ip=10.128.0.5
