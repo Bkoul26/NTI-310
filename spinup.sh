d8981b69ce53fd475f93eee349772f7229b8cb7d
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-03-18 19:18:38 -0700

    Update spinup.sh

--- spinup.sh
+++ spinup.sh
@@ -1,3 +1,4 @@
+#rsyslog
 gcloud compute instances create rsyslog1 \
         --image-family centos-7 \
         --image-project centos-cloud \
@@ -7,8 +8,26 @@ gcloud compute instances create rsyslog1 \
         --scopes cloud-platform \
         --metadata-from-file startup-script=/home/bloshakoul/NTI-310/logserver/logsrv.sh \
         --private-network-ip=10.128.0.5
-
-
+#ldap client
+gcloud compute instances create ldap-client \
+        --image-family centos-7 \
+        --image-project centos-cloud \
+        --zone us-central1-a \
+        --tags "http-server","https-server" \
+        --machine-type f1-micro \
+        --scopes cloud-platform \
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/ldap-client \
+        --private-network-ip=10.128.0.8
+#ldap serv
+gcloud compute instances create ldap-server \
+        --image-family centos-7 \
+        --image-project centos-cloud \
+        --zone us-central1-a \
+        --tags "http-server","https-server" \
+        --machine-type f1-micro \
+        --scopes cloud-platform \
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/ldap-server \
+        --private-network-ip=10.128.0.9
 #nfs client
 gcloud compute instances create nfs-client \
         --image-family centos-7 \
@@ -30,3 +49,37 @@ gcloud compute instances create nfs-server \
         --scopes cloud-platform \
         --metadata-from-file startup-script=/home/bloshakoul/NTI-310/nfs-server \
         --private-network-ip=10.128.0.7
+      
+#postgres
+
+gcloud compute instances create postgres \
+        --image-family centos-7 \
+        --image-project centos-cloud \
+        --zone us-central1-a \
+        --tags "http-server","https-server" \
+        --machine-type f1-micro \
+        --scopes cloud-platform \
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/POSTGRESQL \
+        --private-network-ip=10.128.0.10
+    
+#DJANGO
+gcloud compute instances create django \
+        --image-family centos-7 \
+        --image-project centos-cloud \
+        --zone us-central1-a \
+        --tags "http-server","https-server" \
+        --machine-type f1-micro \
+        --scopes cloud-platform \
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/django \
+        --private-network-ip=10.128.0.11
+        
+#ubuntu 
+gcloud compute instances create client ubuntu \
+        --image-family centos-7 \
+        --image-project centos-cloud \
+        --zone us-central1-a \
+        --tags "http-server","https-server" \
+        --machine-type f1-micro \
+        --scopes cloud-platform \
+        --metadata-from-file startup-script=/home/bloshakoul/NTI-310/client ubuntu \
+        --private-network-ip=10.128.0.12
