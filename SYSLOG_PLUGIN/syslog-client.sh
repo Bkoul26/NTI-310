7d9a8c3a6e5b6e0c8ea4ab09ab74deea26d4ced0
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-05-21 17:34:30 -0700

    Update syslog-client.sh

--- SYSLOG_PLUGIN/syslog-client.sh
+++ SYSLOG_PLUGIN/syslog-client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "*.info;mail.none;authpriv.none;cron.none   @logsrv" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+ /etc/nagios/nrpe.conf
 #Important: this should be the internal not external IP of the server or the dns name of your server.
