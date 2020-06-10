8b40ba64cccae0f921381bb5dd108e775b4b127c
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-06-10 15:04:53 -0700

    Update syslogsrv.sh

--- SYSLOG_PLUGIN/syslogsrv.sh
+++ SYSLOG_PLUGIN/syslogsrv.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
-sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
-sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
-sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
-setenforce 0
-netstat -antup | grep 514
-cat /etc/rsyslog.conf | sort
-state=$(systemctl status rsyslog | grep Active | awk '{print $2}')
+state=$(systemctl status rsyslog | grep -c Active/Inactive/Failed | awk '{print $2}')
 inactive="inactive"
 active="active"
 failed="failed"
+
+
+if [systemctl status rsyslog | grep -c Active = 1]; then
+    echo "STATUS: UP"
+    exit 0;
+elif [systemctl status rsyslog | grep -c Inactive = 1]; then
+    echo "STATUS: WARNING"
+    exit 1;
+elif [systemctl status rsyslog | grep -c Failed = 1]; then
+    echo "STATUS: CRITICAL"
+    exit 2;
+else
+    echo "STATUS: UNKNOWN"
+    exit 3;
+
+
+
+
+
+-----------------------------
 
 if [ $state == $active ]; then
       echo "STATUS:UP"
