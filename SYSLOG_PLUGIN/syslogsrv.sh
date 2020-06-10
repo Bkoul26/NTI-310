eb82f44937e70a74b0352cf133ccdef22a3a2d4d
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2020-06-10 15:27:52 -0700

    Update syslogsrv.sh

--- SYSLOG_PLUGIN/syslogsrv.sh
+++ SYSLOG_PLUGIN/syslogsrv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-state=$(systemctl status rsyslog | grep -c Active/Inactive/Failed | awk '{print $2}')
-inactive="inactive"
+state=$(systemctl status rsyslog | awk '{print $2}')
 active="active"
+inactive="inactive"
 failed="failed"
+
 
 
 if [systemctl status rsyslog | grep -c Active = 1]; then
@@ -17,76 +18,3 @@ elif [systemctl status rsyslog | grep -c Failed = 1]; then
 else
     echo "STATUS: UNKNOWN"
     exit 3;
-
-
-
-
-
------------------------------
-
-if [ $state == $active ]; then
-      echo "STATUS:UP"
-      exit 0;
-
-   elif [ $state == $inactive ]; then
-      echo "STATUS:WARNING"
-      exit 1;
-      
-    elif [ $state == $failed ]; then
-      echo "STATUS:CRITICAL"
-      exit 2;
-
-else
-   echo "STATUS:UNKNOWN"
-   exit 3;
-
-fi
-state=$(systemctl status rsyslog | grep Inactive | awk '{print $3}')
-if [ $state == $active ]; then
-      echo "STATUS:UP"
-      exit 0;
-
-   elif [ $state == $inactive ]; then
-      echo "STATUS:WARNING"
-      exit 1;
-      
-    elif [ $state == $failed ]; then
-      echo "STATUS:CRITICAL"
-      exit 2;
-
-else
-   echo "STATUS:UNKNOWN"
-   exit 3;
-state=$(systemctl status rsyslog | grep Failed | awk '{print $4}')
-if [ $state == $active ]; then
-      echo "STATUS:UP"
-      exit 0;
-
-   elif [ $state == $inactive ]; then
-      echo "STATUS:WARNING"
-      exit 1;
-      
-    elif [ $state == $failed ]; then
-      echo "STATUS:CRITICAL"
-      exit 2;
-
-else
-   echo "STATUS:UNKNOWN"
-   exit 3;
-   netstat -antup | grep 514
-state=$(systemctl status rsyslog | grep UNKNOWN | awk '{print $5}')
-if [ $state == $active ]; then
-      echo "STATUS:UP"
-      exit 0;
-
-   elif [ $state == $inactive ]; then
-      echo "STATUS:WARNING"
-      exit 1;
-      
-    elif [ $state == $failed ]; then
-      echo "STATUS:CRITICAL"
-      exit 2;
-
-else
-   echo "STATUS:UNKNOWN"
-   exit 3;
